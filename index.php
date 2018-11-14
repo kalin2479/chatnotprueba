ff0ed2c552212a6fecf17df5fca2c5838b3b6e6f
Carlos Espinoza <carlos.espinoza24g@gmail.com>
2018-11-14 12:04:28 -0500

    codigo

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-echo "hola mundo";
+
+if (isset($_REQUEST['hub_challenge'])){
+  $challenge = $_REQUEST['hub_challenge'];
+  $token = $_REQUEST['hub_verify_token'];
+}
+
+if ($token == "myCustomToken123"){
+  echo $token;
+}
+
+
 ?>
